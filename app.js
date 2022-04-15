9b5fff2f8c8aa19ff99c22cbc04098cb241eb219
yiyayamaya <892223116@qq.com>
2022-04-14 22:25:05 -0400

    food list

--- app.js
+++ app.js
@@ -4,12 +4,35 @@ const static = express.static(__dirname + '/public');
 
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
+const path = require("path");
+
+
+const handlebarsInstance = exphbs.create({
+  defaultLayout: 'main',
+  // Specify helpers which are only registered on this instance.
+  helpers: {
+    asJSON: (obj, spacing) => {
+      if (typeof spacing === "number")
+        return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
+
+      return new Handlebars.SafeString(JSON.stringify(obj));
+    }
+  },
+  partialsDir: [
+    'views/posts/'
+  ]
+});
 
 app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+//app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+
+app.engine('handlebars', handlebarsInstance.engine);
+
+app.set('views', path.join(__dirname, '/views'));
+
 app.set('view engine', 'handlebars');
 
 configRoutes(app);
@@ -18,3 +41,8 @@ app.listen(3000, () => {
   console.log("We've now got a server!");
   console.log('Your routes will be running on http://localhost:3000');
 });
+
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,184 @@ const customerRoutes = require('./customer');
 const path = require('path');
 
 const constructorMethod = (app) => {
-  app.use('/customer', customerRoutes);
+  //app.use('/customer', customerRoutes);
   //app.use('/users', userRoutes);
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve('static/index.html'));
+    res.render("posts/index");
+  });
+
+  app.get('/foodList', (req, res) => {
+    //res.render("posts/foodList");
+
+    let itemsArray=[[{
+      "_id": "e4464231-9981-4cde-8d09-94bfb632128b",
+      "userProfile": {
+        "username": "clararamos",
+        "firstName": "Clara",
+        "lastName": "Ramos",
+        "email": "cramos1@stevens.edu",
+        "phone": "123-456-7809",
+        "zip": "07030",
+        "_id": "282ec7b5-ebf3-4fef-8ba2-8ea5d1775c9f"
+      },
+      "name": "rice cooker",
+      "categories": [
+        "home",
+        "appliances"
+      ],
+      "description": "a handy machine to cook rice without burning it",
+      "price": 10,
+      "paymentMethod": "cash",
+      "zip": "07030",
+      "imagePath": "/public/uploads/ricecooker.jpg",
+      "time": {
+        "minDays": 0.5,
+        "maxDays": 2
+      },
+      "rentals": [],
+      "status": "available"
+    },{
+      "_id": "e4464231-9981-4cde-8d09-94bfb632128b",
+      "userProfile": {
+        "username": "clararamos",
+        "firstName": "Clara",
+        "lastName": "Ramos",
+        "email": "cramos1@stevens.edu",
+        "phone": "123-456-7809",
+        "zip": "07030",
+        "_id": "282ec7b5-ebf3-4fef-8ba2-8ea5d1775c9f"
+      },
+      "name": "rice cooker",
+      "categories": [
+        "home",
+        "appliances"
+      ],
+      "description": "a handy machine to cook rice without burning it",
+      "price": 10,
+      "paymentMethod": "cash",
+      "zip": "07030",
+      "imagePath": "/public/uploads/ricecooker.jpg",
+      "time": {
+        "minDays": 0.5,
+        "maxDays": 2
+      },
+      "rentals": [],
+      "status": "available"
+    },{
+      "_id": "e4464231-9981-4cde-8d09-94bfb632128b",
+      "userProfile": {
+        "username": "clararamos",
+        "firstName": "Clara",
+        "lastName": "Ramos",
+        "email": "cramos1@stevens.edu",
+        "phone": "123-456-7809",
+        "zip": "07030",
+        "_id": "282ec7b5-ebf3-4fef-8ba2-8ea5d1775c9f"
+      },
+      "name": "rice cooker",
+      "categories": [
+        "home",
+        "appliances"
+      ],
+      "description": "a handy machine to cook rice without burning it",
+      "price": 10,
+      "paymentMethod": "cash",
+      "zip": "07030",
+      "imagePath": "/public/uploads/ricecooker.jpg",
+      "time": {
+        "minDays": 0.5,
+        "maxDays": 2
+      },
+      "rentals": [],
+      "status": "available"
+    },{
+      "_id": "e4464231-9981-4cde-8d09-94bfb632128b",
+      "userProfile": {
+        "username": "clararamos",
+        "firstName": "Clara",
+        "lastName": "Ramos",
+        "email": "cramos1@stevens.edu",
+        "phone": "123-456-7809",
+        "zip": "07030",
+        "_id": "282ec7b5-ebf3-4fef-8ba2-8ea5d1775c9f"
+      },
+      "name": "rice cooker",
+      "categories": [
+        "home",
+        "appliances"
+      ],
+      "description": "a handy machine to cook rice without burning it",
+      "price": 10,
+      "paymentMethod": "cash",
+      "zip": "07030",
+      "imagePath": "/public/uploads/ricecooker.jpg",
+      "time": {
+        "minDays": 0.5,
+        "maxDays": 2
+      },
+      "rentals": [],
+      "status": "available"
+    },{
+      "_id": "e4464231-9981-4cde-8d09-94bfb632128b",
+      "userProfile": {
+        "username": "clararamos",
+        "firstName": "Clara",
+        "lastName": "Ramos",
+        "email": "cramos1@stevens.edu",
+        "phone": "123-456-7809",
+        "zip": "07030",
+        "_id": "282ec7b5-ebf3-4fef-8ba2-8ea5d1775c9f"
+      },
+      "name": "rice cooker",
+      "categories": [
+        "home",
+        "appliances"
+      ],
+      "description": "a handy machine to cook rice without burning it",
+      "price": 10,
+      "paymentMethod": "cash",
+      "zip": "07030",
+      "imagePath": "/public/uploads/ricecooker.jpg",
+      "time": {
+        "minDays": 0.5,
+        "maxDays": 2
+      },
+      "rentals": [],
+      "status": "available"
+    },{
+      "_id": "67846b29-6aba-416f-9fae-4c2df00c4488",
+      "userProfile": {
+        "username": "clararamos",
+        "firstName": "Clara",
+        "lastName": "Ramos",
+        "email": "cramos1@stevens.edu",
+        "phone": "123-456-7809",
+        "zip": "07030",
+        "_id": "282ec7b5-ebf3-4fef-8ba2-8ea5d1775c9f"
+      },
+      "name": "Wii U",
+      "categories": [
+        "appliances",
+        "video_games",
+        "electronics"
+      ],
+      "description": "A Nintendo Wii U",
+      "price": 30,
+      "paymentMethod": "cash",
+      "zip": "07030",
+      "imagePath": "/public/uploads/wiiu.png",
+      "time": {
+        "minDays": 1,
+        "maxDays": 10
+      },
+      "rentals": [],
+      "status": "available"
+    }]];
+
+
+    res.render("posts/foodList", { pageTitle: "List of All Items", itemsArray: itemsArray });
+
+    //res.send("posts/foodList");
   });
 
   app.use('*', (req, res) => {
