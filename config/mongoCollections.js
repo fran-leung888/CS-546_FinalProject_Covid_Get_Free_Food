8636a06575e28ad4d09c5c5b5ce1da128c096816
yiyayamaya <892223116@qq.com>
2022-04-20 01:20:44 -0400

    food search

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -17,5 +17,6 @@ const getCollectionFn = (collection) => {
 
 /* Now, you can list your collections here: */
 module.exports = {
-    posts: getCollectionFn('posts')
+    posts: getCollectionFn('items'),
+    food: getCollectionFn('Food')
 };
