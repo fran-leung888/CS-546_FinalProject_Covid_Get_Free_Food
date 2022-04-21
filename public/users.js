78d09e3fd248ec5ce174b47262e4273f03f46416
ChaseWangPluviophile <101293062+ChaseWangPluviophile@users.noreply.github.com>
2022-04-21 17:25:12 -0400

    Add files via upload

---
+++ public/users.js
@@ -0,0 +1,20 @@
+const mongoCollections = require('../config/mongoCollections');
+const customers = mongoCollections.posts;
+const uuid = require('uuid/v4');
+
+const exportedMethods = {
+  async getAllPosts() {
+    const postCollection = await customers();
+    return await postCollection.find({}).toArray();
+  },
+
+  async getUserById() {
+    
+  }
+
+};
+
+
+
+
+module.exports = exportedMethods;
