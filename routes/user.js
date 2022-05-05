f6f4ff06d0864d12eafe540bc202dec236c7bd6b
yiyayamaya <892223116@qq.com>
2022-05-05 16:58:56 -0400

    fix

--- routes/user.js
+++ routes/user.js
@@ -125,6 +125,7 @@ router.get('/history', async (req, res) => {
         return res.redirect("/login");
 
     }
+    const user = await userData.getUserById(req.session.user.id);
     res.render("posts/userHistory", {user: user});
 
 
